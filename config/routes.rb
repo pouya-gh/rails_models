01def529dbb9766c0e5924882759515da75662bd
Pouya Gharib Pour <p.gharibpour@gmail.com>
2015-03-04 11:21:43 +0330

    end of rails views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ModelTest::Application.routes.draw do
+  resources :posts, only: [:index, :show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
