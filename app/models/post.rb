9c31034a7af68feef2e3b906b73ec3b2b5eaed39
Pouya Gharib Pour <p.gharibpour@gmail.com>
2015-05-04 18:58:41 +0430

    add User to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
+  belongs_to :user
+
   validates :title, presence: true
   validates :body, length: {minimum: 5, maximum: 500}
+  validates :user_id, presence: true
 end
