f61fc6e27036b5f8ee3cad0e9ab8cb248590e523
Pouya Gharib Pour <p.gharibpour@gmail.com>
2015-07-18 15:21:30 +0430

    add a callback to omit whitespaces from title

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,11 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :body, length: {minimum: 5, maximum: 500}
   validates :user_id, presence: true
+
+  before_validation :omit_whitespaces_from_title
+
+  private
+  def omit_whitespaces_from_title
+    self.title = self.title.chomp
+  end
 end
