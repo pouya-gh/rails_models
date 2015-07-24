295d5c3812ce4705f60ffa820f686ced1dc492f9
Pouya Gharib Pour <p.gharibpour@gmail.com>
2015-07-24 15:32:23 +0430

    run the new migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150504142301) do
+ActiveRecord::Schema.define(version: 20150724105801) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20150504142301) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password"
   end
 
 end
