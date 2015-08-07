9e1ed9ca02393831d4727c6a66f5f912dbf27e6e
Pouya Gharib Pour <p.gharibpour@gmail.com>
2015-08-07 10:38:05 +0430

    add current_user method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,14 @@
 class PostsController < ApplicationController
   layout 'layouts/my_layout', only: [:show]
-  before_filter :find_post_by_id, only: [:show, :edit, :update, :destroy]
   before_filter :check_signed_in, only: [:new, :create, :edit, :update, :destroy]
+  before_filter :find_post_by_id, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all
   end
 
   def show
-    # @post = Post.find(params[:id])
+    @post = Post.find(params[:id])
   end
 
   def new
@@ -16,30 +16,24 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new
+    @post = current_user.posts.new()
     @post.title = params[:post][:title]
     @post.body = params[:post][:body]
-    @post.user_id = params[:post][:user_id]
     @post.save
     redirect_to posts_path
   end
 
   def edit
-    # @post = Post.find(params[:id])
-    #render :new
   end
 
   def update
-    # @post = Post.find(params[:id])
     @post.title = params[:post][:title]
     @post.body = params[:post][:body]
-    @post.user_id = params[:post][:user_id]
     @post.save
     redirect_to posts_path
   end
 
   def destroy
-    # post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
   end
@@ -47,7 +41,7 @@ class PostsController < ApplicationController
   private
 
   def find_post_by_id
-    @post = Post.find(params[:id])
+    @post = current_user.posts.find(params[:id])
   end
 
   def check_signed_in
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,4 +1,7 @@
 module SessionsHelper
+  def current_user
+    @current_user = @current_user || User.find_by_id(cookies[:user_id])
+  end
   def signed_in?
     if cookies[:user_id]
       User.find_by_id(cookies[:user_id])
