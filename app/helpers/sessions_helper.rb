be70962139cbb9ec1f1452e6ccf16d0f9d477292
Pouya Gharib Pour <p.gharibpour@gmail.com>
2015-08-07 10:38:42 +0430

    refactor signed_in? method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,11 +2,8 @@ module SessionsHelper
   def current_user
     @current_user = @current_user || User.find_by_id(cookies[:user_id])
   end
+  
   def signed_in?
-    if cookies[:user_id]
-      User.find_by_id(cookies[:user_id])
-    else
-      false
-    end
+    current_user
   end
 end
