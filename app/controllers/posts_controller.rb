d5cbcc7b727aa89af35fd2c551b978f211aa76ca
Pouya Gharib Pour <p.gharibpour@gmail.com>
2015-09-05 10:27:05 +0430

    add XML and JSON API

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   layout 'layouts/my_layout', only: [:show]
   before_filter :check_signed_in, only: [:new, :create, :edit, :update, :destroy]
   before_filter :find_post_by_id, only: [:edit, :update, :destroy]
+  skip_before_action :verify_authenticity_token, only: [:create]
 
   def index
     @posts = Post.all
@@ -9,6 +10,11 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json { render json: @post }
+      format.xml  { render xml:  @post }
+    end
   end
 
   def new
@@ -19,8 +25,20 @@ class PostsController < ApplicationController
     @post = current_user.posts.new()
     @post.title = params[:post][:title]
     @post.body = params[:post][:body]
-    @post.save
-    redirect_to posts_path
+    if @post.save
+      respond_to do |format|
+        format.html { redirect_to posts_path }
+        format.json { head 200 }
+        format.xml  { head 200 }
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to posts_path }
+        format.json { render "create", status: 400 }
+        format.xml  { head 400 }
+      end
+    end
+    # redirect_to posts_path
   end
 
   def edit
